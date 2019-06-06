c981e1522665315895b0974bf861ca2bd9624f36
Shobhit <hathishobhit@gmail.com>
2019-06-06 10:19:21 -0700

    modified run script

--- text/run_script.py
+++ text/run_script.py
@@ -1,2 +1,4 @@
-python run.py --model dwac --ood-class yelp --max-epochs 5
-tar -cvzf data/dwac_yelp data/temp
+python run.py --model dwac --ood-class stack_overflow --max-epochs 5
+tar -cvzf data/dwac_stack_overflow data/temp
+python run.py --model proto ood-class stack_overflow --max-epochs 5
+tar -cvzf data/proto_stack_overflow data/temp
